87a8a4bb2be07d6d0d38693cff5b062bc5486c9c
Ethan Wang <70038315+ewang223@users.noreply.github.com>
2026-06-05 17:12:30 -0700

    inventory system mostly done
    Bug where localinventory appears to be populated twice, items not usable from inventory yet

--- Assets/Scripts/Game/Inventory/InventoryManager.cs
+++ Assets/Scripts/Game/Inventory/InventoryManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
 using FishNet.Component.Prediction;
+using System.Collections.Generic;
+using GameKit.Dependencies.Utilities;
 
 
 public class InventoryManager : NetworkBehaviour
@@ -18,15 +20,16 @@ public class InventoryManager : NetworkBehaviour
     InputAction selectSlotThree;
     InputAction selectSlotFour;
 
-    public readonly SyncVar<ItemSO[]> items = new SyncVar<ItemSO[]>();
+    public int invSize;
+    public readonly SyncList<ItemSO> items = new SyncList<ItemSO>();
 
-    public ItemSO[] localItems = new ItemSO[4];
+    public List<ItemSO> localItems = new List<ItemSO>();
     public int selectedSlot; //player currently held Item
     public ItemSO selectedItem; //player currently held Item
 
     public void Awake()
     {
-        items.Value = new ItemSO[4];
+        
         //set keybinds
         dropItem = inputs.FindAction("Drop");
         scrollWheel = inputs.FindAction("ScrollWheel");
@@ -38,16 +41,61 @@ public class InventoryManager : NetworkBehaviour
 
         dropItem.canceled += DropItem;
         scrollWheel.performed += ScrollWheel;
+        selectSlotOne.canceled += SelectSlotOne;
+        selectSlotTwo.canceled += SelectSlotTwo;
+        selectSlotThree.canceled += SelectSlotThree;
+        selectSlotFour.canceled += SelectSlotFour;
+
+
+        items.OnChange += OnInventoryChange;
     }
 
-    void Update()
-    { 
-        //localItems = items.Value; //purely for the sake of testing. Delegates dont work on syncvar arrays, they work on synclists, but i tried a synclist and it was really fucked up so i gave up
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        if (items.Count >= invSize) //return if inv has already been populated
+            return;
+        //populate inventory until it's the correct size on server
+        
+        for (int i = 0; i < invSize; i++)
+        {
+            items.Add(null);
+        }
+    }
+
+    private void OnInventoryChange(SyncListOperation op, int index, ItemSO oldItem, ItemSO newItem, bool asServer)
+    {
+        switch (op)
+        {
+            case SyncListOperation.Add:
+                Debug.Log("added to inv!");
+                localItems.Add(newItem);
+                break;
+            case SyncListOperation.Insert:
+                // newItem was inserted at [index]
+                localItems.Insert(index, newItem);
+                break;
+            case SyncListOperation.Set:
+                localItems[index] = newItem;
+                // oldItem was replaced by newItem at [index]
+                break;
+            case SyncListOperation.RemoveAt:
+                localItems.RemoveAt(index);
+                // oldItem was removed from [index]
+                break;
+            case SyncListOperation.Clear:
+                localItems.Clear();
+                // The list was entirely cleared
+                break;
+            
+        }
+        selectedItem = items[selectedSlot];
     }
 
     public void DropItem(InputAction.CallbackContext c)
     {
-        TryDropItem(selectedSlot, selectedItem);
+        TryDropItem(selectedSlot, selectedItem); // so technically selected item does like nothing but i have it just in case
     }
 
     public void ScrollWheel(InputAction.CallbackContext c)
@@ -60,27 +108,95 @@ public class InventoryManager : NetworkBehaviour
         {
             selectedSlot = 0;
         }
-        if (selectedSlot >= items.Value.Length)
+        if (selectedSlot >= items.Count)
         {
-            selectedSlot = items.Value.Length - 1;
+            selectedSlot = items.Count - 1;
         }
+        selectedItem = items[selectedSlot];
     }
 
+    public void SelectSlotOne(InputAction.CallbackContext c)
+    {
+        selectedSlot = 0;
+        selectedItem = items[0];
+    }
+
+    public void SelectSlotTwo(InputAction.CallbackContext c)
+    {
+        selectedSlot = 1;
+        selectedItem = items[1];
+    }
     
+    public void SelectSlotThree(InputAction.CallbackContext c)
+    {
+        selectedSlot = 2;
+        selectedItem = items[2];
+    }
+
+    public void SelectSlotFour(InputAction.CallbackContext c)
+    {
+        selectedSlot = 3;
+        selectedItem = items[3];
+    }
 
 
+    [ServerRpc(RequireOwnership = false)] 
+    public void TryPickUpItem(ItemSO item, NetworkObject caller) //if this runs, the object will be destroyed, so it cant be called anymore from server (this is the validity check)
+    {
+        if (!caller)//check if the object which called this method exists + is spawned on server
+        {
+            return;
+        } else
+        {
+            if (!caller.IsSpawned)
+            {
+                return;
+            }
+        }
+
+        //check for stackable items, if theres a stack they can stack onto
+        if (item.stackable)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i])
+                {
+                    if (items[i].inventoryItemPrefab == item.inventoryItemPrefab && items[i].count < items[i].stackMax)
+                    {
+                        AddItemToStack(i, item);
+                        Despawn(caller);
+                        return;
+                    }
+                }
+                
+            }
+        }
+        //otherwise, check for first open spot
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (!items[i])
+            {
+                AddItem(i, item);
+                Despawn(caller);
+                return;
+            }
+        }
+
+        //inventory full
+    }
 
     [ServerRpc(RequireOwnership = false)]
     public void TryDropItem(int index, ItemSO item)
     {
-        if (items.Value[index] )
+        if (items[index])
         {
-            if (items.Value[index].cursed) //cursed items cant be dropped normally
+            if (items[index].cursed) //cursed items cant be dropped normally
             {
                 return;
             }
+            
+            SpawnItemInteractable(items[index]); //first spawn item from inventory, then remove it from inventory
             RemoveItem(index, item);
-            SpawnItemInteractable(item);
         }
     }
 
@@ -88,29 +204,40 @@ public class InventoryManager : NetworkBehaviour
     [Server]
     public void AddItem(int index, ItemSO item) //add item to empty inventory slot
     {
-        items.Value[index] = item;
+        items[index] = item;
+        items.Dirty(index);
     }
 
     [Server]
     public void AddItemToStack(int index, ItemSO item) //adds to a stack
     {
-        items.Value[index].count++;
+        items[index].count++;
+        items.Dirty(index);
     }
 
     [Server]
     public void RemoveItem(int index, ItemSO item) //removes the item, or one stack of the item
     {
-        if (!items.Value[index])
+        if (!items[index])
         {
             return;
         }
-        if (items.Value[index].stackable)
+        if (items[index].stackable)
         {
-            
+            if (items[index].count <= 1)
+            {
+                items[index] = null;
+                items.Dirty(index);
+            }
+            else
+            {
+                items[index].count--;
+                items.Dirty(index);
+            }
         } else
         {
-            Destroy(items.Value[index]);
-            items.Value[index] = null;
+            items[index] = null;
+            items.Dirty(index);
         }
         
     }
@@ -118,6 +245,10 @@ public class InventoryManager : NetworkBehaviour
     [Server]
     public void SpawnItemInteractable(ItemSO item) //removes the item, or one stack of the item
     {
+        if (!item)
+        {
+            return;
+        }
         GameObject itemInstance = Instantiate(item.itemInteractablePrefab);
         itemInstance.transform.position = transform.position;//spawns it directly on the player, since inventory manager is attached to the player
         
--- Assets/Scripts/Items/ItemInteractable.cs
+++ Assets/Scripts/Items/ItemInteractable.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FishNet.Object;
 using UnityEngine;
 
@@ -9,45 +10,10 @@ public class ItemInteractable : Interactable
     public override void Interact()
     {
         InventoryManager inv = player.GetComponentInChildren<InventoryManager>();
-        TryPickUpItem(itemSO, inv); 
+        inv.TryPickUpItem(itemSO, base.NetworkObject); 
     }
 
-    [ServerRpc(RequireOwnership = false)] 
-    public void TryPickUpItem(ItemSO item, InventoryManager inv) //if this runs, the object will be destroyed, so it cant be called anymore from server (this is the validity check)
-    {
-        //get current client's item inventory
-        ItemSO[] items = inv.items.Value;
-
-        //check for stackable items, if theres a stack they can stack onto
-        if (item.stackable)
-        {
-            for (int i = 0; i < items.Length; i++)
-            {
-                if (items[i])
-                {
-                    if (items[i].inventoryItemPrefab == item.inventoryItemPrefab && items[i].count < items[i].stackMax)
-                    {
-                        inv.AddItemToStack(i, item);
-                        Despawn(gameObject);
-                        return;
-                    }
-                }
-                
-            }
-        }
-        //otherwise, check for first open spot
-        for (int i = 0; i < items.Length; i++)
-        {
-            if (!items[i])
-            {
-                inv.AddItem(i, item);
-                Despawn(gameObject);
-                return;
-            }
-        }
-
-        //inventory full
-    }
+    
 
 
     
