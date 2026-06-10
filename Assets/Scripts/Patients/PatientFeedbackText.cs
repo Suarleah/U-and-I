951a45d177ed465500ec1073bed8a4e90a219aac
Ethan Wang <70038315+ewang223@users.noreply.github.com>
2026-06-10 14:55:06 -0700

    made patientfeedback code less offensive

--- Assets/Scripts/Patients/PatientFeedbackText.cs
+++ Assets/Scripts/Patients/PatientFeedbackText.cs
@@ -2,27 +2,50 @@ using UnityEngine;
 using TMPro;
 using FishNet;
 using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using System.Collections;
 
 public class PatientFeedbackText : NetworkBehaviour
 {
-    //when you interact with a Patient, they might give feedback on your interaction, this is the visual element of that
-    [ObserversRpc]
-    public void Begin()
+    public readonly SyncVar<float> alpha = new SyncVar<float>();
+
+    void Awake()
     {
-        gameObject.SetActive(true);
-        gameObject.GetComponentInChildren<TextMeshProUGUI>().alpha = 1; //just temp, its to set opacity to 0
-        
+        alpha.OnChange+= OnChange;
     }
 
-    [ObserversRpc]
+    //when you interact with a Patient, they might give feedback on your interaction, this is the visual element of that
+    [Server]
+    public void Begin()
+    {
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().alpha = 1; //just temp, its to set opacity to 0
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
+        if (IsServerStarted)
+        {
+            ServerUpdate();
+        }
+        
+    }
+
+    [Server]
+    void ServerUpdate(){
         transform.position += Time.deltaTime * (new Vector3(0, 1, 0));
-        gameObject.GetComponentInChildren<TextMeshProUGUI>().alpha -= Time.deltaTime;
         if (gameObject.GetComponentInChildren<TextMeshProUGUI>().alpha <= 0)
         {
             Despawn(base.NetworkObject);
+            return;
         }
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().alpha -= Time.deltaTime;
+    }
+
+    public void OnChange(float prev, float next, bool asServer)
+    {
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().alpha = next;
     }
 }
