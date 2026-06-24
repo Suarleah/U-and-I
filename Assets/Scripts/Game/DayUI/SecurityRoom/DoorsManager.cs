d974e44009677774bab72c5f7295ac3eb1a852af
Ethan Wang <70038315+ewang223@users.noreply.github.com>
2026-06-24 16:15:00 -0700

    s

--- Assets/Scripts/Game/DayUI/SecurityRoom/DoorsManager.cs
+++ Assets/Scripts/Game/DayUI/SecurityRoom/DoorsManager.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorsManager : MonoBehaviour
 {
     //this is the script that manages the doors (ie which doors are on cooldown at any given time, which doors are opened/closed for the janitor)
+   
+   public List<bool> doors = new List<bool>(); //keeps track of what doors are down
+
 }
