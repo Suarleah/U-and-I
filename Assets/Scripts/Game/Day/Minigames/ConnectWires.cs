821d42944020c4ead62228b48c9ec21222e14dfa
02Din02 <dannykloneckilmp@gmail.com>
2026-09-04 13:49:35 -0500

    Starting new wires

--- Assets/Scripts/Game/Day/Minigames/ConnectWires.cs
+++ Assets/Scripts/Game/Day/Minigames/ConnectWires.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class ConnectWires : MinigameBase
 {
 
-    [Header("Track Following")]
+    [Header("Minigame")]
     public static ConnectWires Instance;
-    public LayerMask trackLayer;
-    public LayerMask trackEnd;
     public Camera cam;
-    public float offTrackGraceTime = 0.15f;
-
-    bool tracking = false;
-    bool hasEntered = false;
-    float offTrackTimer = 0f;
-    bool hasPenalized = false;
+    public Vector3 mousePos;
+    public bool isDragging = false;
 
     [Header("Cursor UI")]
     public RectTransform cursorUI; private Animator handAnim;
@@ -28,17 +23,12 @@ public class ConnectWires : MinigameBase
 
         handAnim = cursorUI.GetComponent<Animator>();
 
-        Physics2D.queriesHitTriggers = true; // this is some bullshit bro
-
         gameObject.SetActive(false);
     }
 
     public override void Open(PatientInteractionInfo info)
     {
         base.Open(info);
-        tracking = true;
-        hasEntered = false;
-        offTrackTimer = 0f;
         score = 6;
     }
 
@@ -54,70 +44,31 @@ public class ConnectWires : MinigameBase
             return;
         }
 
-        cursorUI.position = mouseWorld;
+        mousePos = mouseWorld;
+        cursorUI.position = mousePos;
 
-        if (!tracking) 
+    }
+
+    public void CursorEnteredWireStart(int index) // Wire w
+    {
+        if (!isDragging)
         {
             return;
         }
 
-        bool onTrackNow = Physics2D.OverlapPoint(mouseWorld, trackLayer) != null;
-
-        if (!hasEntered)
-        {
-            //Wait until they enter
-            if (onTrackNow)
-            {
-                hasEntered = true;
-                offTrackTimer = 0f;
-            }
-            return;
-        }
-
-        if (onTrackNow)
-        {
-            offTrackTimer = 0f;
-            hasPenalized = false;
-        }
-        else
-        {
-            offTrackTimer += Time.deltaTime;
-            if (offTrackTimer >= offTrackGraceTime && !hasPenalized)
-            {
-                OnLeftTrack();
-                hasPenalized = true;
-            }
-        }
-
-    if (Physics2D.OverlapPoint(mouseWorld, trackEnd) != null)
-        {
-            OnEndTrack();
-        }
 
     }
-
-    public void OnLeftTrack() // when you suck at the game
+    public void OnDragWire()
     {
-        score -= 1;
-        handAnim.SetTrigger("hurt");
-
-        if (score <= 0)
-        {
-            OnWireResult(0); // Mega lose
-            return;
-        }
-
-        offTrackTimer = 0f;
+        isDragging = true;
+    }
+    public void OnEndDragWire()
+    {
+        isDragging = false;
     }
 
-    public void OnEndTrack() // when you get to the end
+    public void OnGameResult(int result)
     {
-        OnWireResult(score);
-    }
-
-    public void OnWireResult(int result)
-    {
-        tracking = false;
         Finish(result);
     }
 }
