078c2b9fe82d63a4f29814da651b86127aa05809
02Din02 <dannykloneckilmp@gmail.com>
2026-09-17 14:17:47 -0500

    I am a bad friend

--- Assets/Scripts/Game/Day/Minigames/Wires/ConnectWires.cs
+++ Assets/Scripts/Game/Day/Minigames/Wires/ConnectWires.cs
@@ -6,73 +6,80 @@ using UnityEngine.UI;
 
 public class ConnectWires : MinigameBase
 {
-
     [Header("Minigame")]
     public static ConnectWires Instance;
     public Camera cam;
-    
+
     public Vector3 mousePos;
     public bool isDragging = false;
-    public Wire wireHovering;
-    public int wireCount; //difficulty
+    public Wire wireHovering;   // whatever wire the cursor is currently over
+    public Wire draggingWire;   // the actual start wire the player picked up
+    public int wireCount = 4;   // difficulty
 
     [Header("Wire Spawn")]
     public Transform start;
     public Transform end;
-    // Needed to Spawn wire prefab as children
     public GameObject wirePrefab;
-    public Color[] wireColors; // idk how to get a random normal color
-
+    public Color[] wireColors;
 
     [Header("Cursor UI")]
     public RectTransform cursorUI; private Animator handAnim;
     public RectTransform canvasRect;
 
-    private int score;
+    private int correctConnections;
 
     void Awake()
     {
-        Instance = this;
+        Instance = this; // set the singleton so other scripts can grab this easily
 
-        handAnim = cursorUI.GetComponent<Animator>();
+        handAnim = cursorUI.GetComponent<Animator>(); // grab the animator off the cursor UI
 
-        gameObject.SetActive(false);
+        gameObject.SetActive(false); // start off disabled until the minigame is opened
     }
 
     public override void Open(PatientInteractionInfo info)
     {
         base.Open(info);
-        score = 6;
+        correctConnections = 0; // reset progress every time the minigame opens
         SpawnWires();
     }
 
     private void SpawnWires()
     {
-        List<Color> geniusCoding = wireColors.ToList();
+        List<Color> colorPool = wireColors.ToList();
         // Temporary list of all potential colors copied from a set array when spawning wires
 
-        for (int i = 0; i < wireCount; i++) // wireCount is a public int that should be changed by the difficulty (defualt 4)
+        for (int i = 0; i < wireCount; i++) // wireCount is a public int that should be changed by the difficulty (default 4)
         {
-            int x = Random.Range(0, geniusCoding.Count - 1); // get a random color
+            int x = Random.Range(0, colorPool.Count); // get a random color
 
             GameObject s = Instantiate(wirePrefab, start);
             GameObject e = Instantiate(wirePrefab, end);
             // Instantiate 2 copies of the wire object, one as a child of the start area and the other of the end area
 
-            s.GetComponent<Image>().color = geniusCoding[x];
-            e.GetComponent<Image>().color = geniusCoding[x];
-            // Set the color of both wires to the random color chosen earlier
+            Wire sWire = s.GetComponent<Wire>(); // Wire component off the start wire
+            Wire eWire = e.GetComponent<Wire>(); // c Wire component off the end wire
 
-            s.GetComponent<LineRenderer>().startColor = geniusCoding[x]; s.GetComponent<LineRenderer>().endColor = geniusCoding[x];
-            e.GetComponent<LineRenderer>().startColor = geniusCoding[x]; e.GetComponent<LineRenderer>().endColor = geniusCoding[x];
+            s.GetComponent<Image>().color = colorPool[x];
+            e.GetComponent<Image>().color = colorPool[x];
+            // Set the color of both wires to the SAME random color chosen earlier
+
+            LineRenderer sLine = s.GetComponent<LineRenderer>(); // grab the start wire's line renderer
+            LineRenderer eLine = e.GetComponent<LineRenderer>(); // grab the end wire's line renderer
+            sLine.startColor = colorPool[x]; sLine.endColor = colorPool[x];
+            eLine.startColor = colorPool[x]; eLine.endColor = colorPool[x];
             // Set the line (that shows up between cursor and wire when dragging) to be the same color as the wire itself
 
-            e.GetComponent<Wire>().isStart = false;
+            sWire.colorIndex = x; // store the color index on the start wire so we can match it later
+            eWire.colorIndex = x; // store the same color index on the end wire
+            sWire.connectWires = this; // give the wire a reference back to this manager
+            eWire.connectWires = this; // same for the end wire
+
+            eWire.isStart = false;
             // Set the wire that was spawned in the end area to not be a starting wire (because it is an ending wire)
 
-            geniusCoding.RemoveAt(x);
+            colorPool.RemoveAt(x);
             // Remove the color from the temp list so it cannot be repeated this spawn cycle
-
         }
     }
 
@@ -85,33 +92,53 @@ public class ConnectWires : MinigameBase
 
         if (!RectTransformUtility.ScreenPointToWorldPointInRectangle(canvasRect, Mouse.current.position.ReadValue(), cam, out Vector3 mouseWorld))
         {
-            return;
+            return; // if mouse not on screen
         }
 
-        mousePos = mouseWorld;
-        cursorUI.position = mousePos;
+        mousePos = mouseWorld; // save the mouse's world position for other scripts to use
+        cursorUI.position = mousePos; // move the cursor UI to follow the mouse
 
     }
 
-    public void CursorEnteredWireStart(int index) // Wire w
+    public void BeginDrag(Wire w) // call this from a start wire's PointerDown
+    {
+        if (w == null || !w.isStart || w.isConnected) return; // ignore clicks that aren't a valid, unconnected start wire
+
+        isDragging = true; // flag that a drag has started
+        draggingWire = w; // remember which wire is being dragged
+    }
+
+    private void EndDrag() // onpointerup
     {
         if (!isDragging)
         {
             return;
         }
-    }
-    public void OnDragWire()
-    {
-        isDragging = true;
-    }
-    public void OnEndDragWire()
-    {
-        isDragging = false;
 
-        if (wireHovering != null)
+
+        isDragging = false; // drag is over either way
+
+        if (draggingWire != null && wireHovering != null
+            && !wireHovering.isStart && !wireHovering.isConnected
+            && wireHovering.colorIndex == draggingWire.colorIndex) // check we dropped on the MATCHINNG end wire
         {
-            if (wireHovering.isStart);
+            draggingWire.Connect(wireHovering.transform.position); // lock the start wire's line onto the end wire
+            wireHovering.Connect(wireHovering.transform.position); // mark the end wire as connected too
+
+
+            correctConnections++; // count this as a correct match
+
+            if (correctConnections >= wireCount) // check if every wire has been matched
+            {
+                OnGameResult(1); // win the minigame
+            }
         }
+        else
+        {
+            draggingWire?.ResetLine(); // snap the line back if it wasn't dropped on the right wire
+        }
+
+        draggingWire = null; // clear the dragging reference either way
     }
 
     public void OnGameResult(int result)
--- Assets/Scripts/Game/Day/Minigames/Wires/Wire.cs
+++ Assets/Scripts/Game/Day/Minigames/Wires/Wire.cs
@@ -3,45 +3,63 @@ using UnityEngine;
 public class Wire : MonoBehaviour
 {
     public ConnectWires connectWires;
-    public int index;
+    public int colorIndex; // used to check if this wire matches the one it's being connected to
     public bool isStart = true;
+    public bool isConnected = false;
+
     private LineRenderer lineRenderer;
+
     void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
     }
 
+    void Start()
+    {
+        lineRenderer.SetPosition(0, Vector3.zero); // (0,0,0) = wire is at object center because LOCAL
+        lineRenderer.SetPosition(1, Vector3.zero); //both 0,0,0 = not visible
+    }
+
     void Update()
     {
-        DrawLine();
+        if (connectWires.draggingWire == this) // only follow the mouse if this specific wire is the one being dragged
+        {
+            Vector3 localMouse = transform.InverseTransformPoint(connectWires.mousePos); // convert world mousePos into this wire's local space
+            lineRenderer.SetPosition(1, localMouse); // stretch the line to the cursor
+        }
     }
 
-    void DrawLine()
+    public void OnCursorEnter() // hook to EventTrigger PointerEnter
     {
-        if (!isStart)
-        {
-            return;
-        }
-        lineRenderer.SetPosition(1, connectWires.mousePos);
+        connectWires.wireHovering = this; // tell them this wire is being hovered
     }
 
-    public void OnCursorEnter()
+    public void OnCursorExit() // hook to EventTrigger PointerExit
     {
-        if (!connectWires.isDragging)
+        if (connectWires.wireHovering == this) // only clear it if we're still the one being tracked
         {
-            return;
+            connectWires.wireHovering = null;
         }
-
-        connectWires.wireHovering = this;
+           
     }
 
-    public void OnCursorExit()
+    public void OnPointerDownWire() // OnPointerDown
     {
-        if (!connectWires.isDragging)
-        {
-            return;
-        }
+        connectWires.BeginDrag(this); // try to start a drag from this wire
+    }
 
-        connectWires.wireHovering = null;
+    public void Connect(Vector3 endPosition) // endPosition is passed in as a world-space position of my sister
+    {
+        isConnected = true; // lock this wire so it can't be drugged/matched again
+
+        Vector3 localEnd = transform.InverseTransformPoint(endPosition); // convert to local space before applying
+        // where has this method been my entire life
+
+        lineRenderer.SetPosition(1, localEnd); // snap the line onto the matched wire's position
+    }
+
+    public void ResetLine()
+    {
+        lineRenderer.SetPosition(1, Vector3.zero); // pull the line back to sit on the wire itself (so it not visible)
     }
 }
